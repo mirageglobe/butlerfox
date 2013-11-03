5ccbcffe9ee473a66fdccb936f02b7c04f114b73
Jimmy MG Lim <mirageglobe@gmail.com>
2013-11-03 14:37:45 +0000

    added reboot as machine gone a bit slow after all the updates.

--- pvscript.sh
+++ pvscript.sh
@@ -74,6 +74,7 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
   DEBIAN_FRONTEND=noninteractive apt-get -y --force-yes install mysql-server
   #have funny error when mysql server install. need to check better force install -works fine as normal script
   sleep 2
+  reboot
   echo "Installing ... DB Services MongoDB"
   sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
   echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" >> /etc/apt/sources.list.d/mongodb.list
