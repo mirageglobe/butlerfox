178d5e07d92f59d4a1838996c9796c7f9e57c700
Jimmy MG Lim <mirageglobe@gmail.com>
2014-01-11 03:07:27 +0000

    added some specs of ninja

--- samurai.py
+++ samurai.py
@@ -4,78 +4,83 @@
 # author website - http://www.mirageglobe.com
 # project - http://www.dracoturtur.com/samurai
 
-
 import os, platform, sys
 
+# Samurai aims to be a fire and forget system orchestrating tool. It then summarises and reports the status of each.
+# Samurai is pure simplistic python with no dependancies.
 # Samurai command is interactive
 # Ninja command is invisible non interactive
+# http://www.python.org/dev/peps/pep-0008/#introduction
+# 4 spaces used as indentation
 
+ninjacmd = { 
+    0:  '',
+    1:  'clear',
+    11: 'sudo apt-get update && apt-get upgrade && apt-get autoremove', 
+    12: 'sudo apt-get dist-upgrade',
+    13: 'sudo apt-get install build-essential',
+    30: 'sudo apt-get install sqlite',
+    31: 'sudo apt-get install nginx',
+    999: ''
+}
 
-ninjacmd 		= {	
-					0:	'',
-					1:	'clear',
-					11: 'sudo apt-get update && apt-get upgrade && apt-get autoremove', 
-					12: 'sudo apt-get dist-upgrade',
-					13: 'sudo apt-get install build-essential',
-					30: 'sudo apt-get install sqlite',
-					31: 'sudo apt-get install nginx',
-					999: ''
-					}
+samuraicmd = { 
+    0:  '',
+    1:  'clear',
+    11: 'sudo apt-get update && apt-get upgrade && apt-get autoremove', 
+    12: 'sudo apt-get dist-upgrade',
+    13: 'sudo apt-get install build-essential',
+    30: 'sudo apt-get install sqlite',
+    31: 'sudo apt-get install nginx',
+    999: ''
+}
 
-samuraicmd 		= {	
-					0:	'',
-					1:	'clear',
-					11: 'sudo apt-get update && apt-get upgrade && apt-get autoremove', 
-					12: 'sudo apt-get dist-upgrade',
-					13: 'sudo apt-get install build-essential',
-					30: 'sudo apt-get install sqlite',
-					31: 'sudo apt-get install nginx',
-					999: ''
-					}
+samuraidesc = { 
+    0:  'Exit System',
+    1:  'Clear this screen',
+    11: 'Update ubuntu and cleanup', 
+    12: 'Update ubuntu distribution',
+    13: 'Install build-essential',
+    30: 'Install SQlite',
+    31: 'Install Nginx',
+    999: 'Tell me a joke.. Samurai'
+}
 
-samuraidesc 	= {	
-					0:	'Exit System',
-					1:	'Clear this screen',
-					11: 'Update ubuntu and cleanup', 
-					12: 'Update ubuntu distribution',
-					13: 'Install build-essential',
-					30: 'Install SQlite',
-					31: 'Install Nginx',
-					999: 'Tell me a joke.. Samurai'
-					}
-
-samurairespond 	= {	
-					0:	'See you later Sensei ... Goodbye',
-					1:	'Screen Cleared',
-					11: 'Command is done',
-					12: 'Command is done',
-					13: 'Command is done',
-					30: 'Command is done',
-					999: 'Joke this! I am serious.'
-					}
+samurairespond  = { 
+    0:  'See you later Sensei ... Goodbye',
+    1:  'Screen Cleared',
+    11: 'Command is done',
+    12: 'Command is done',
+    13: 'Command is done',
+    30: 'Command is done',
+    999: 'Joke this! I am serious.'
+}
 
 
 def loadoptions():
-	print("==================================")	
-	print("Samurai is ready...")
-	print("Detected System: {0} ({1})".format(platform.system(),platform.release()))
-	print("==================================")
+    print("==================================") 
+    print("Samurai is ready...")
+    print("Detected System: {0} ({1})".format(platform.system(),platform.release()))
+    print("==================================")
 
-	for key, value in samuraidesc.items():
-		print("[", key, "] -", value)
+    for key, value in samuraidesc.items():
+        print("[", key, "] -", value)
 
 def runcommand(cmdstring):
-	return_value = os.system(cmdstring)
-	#if return_value == 0:
-		#print("Command success.")
+    return_value = os.system(cmdstring)
+    #if return_value == 0:
+        #print("Command success.")
 
 # ===============================
 # Finding some commmands
 # ===============================
 
 for arg in sys.argv:
-	print(arg)
+    print(arg)
 # Adding ninja mode soon.
+# In ninja mode, the commands are not executed; until the end. if you run ninja, it will create a scroll.sh with bash commands which can be used with vagrant
+#export DEBIAN_FRONTEND=noninteractive
+#apt-get -y install package1 package2
 
 # ===============================
 # Default load of system
@@ -89,25 +94,25 @@ loadoptions()
 # ===============================
 
 while gloop == True:
-	#print(chr(27) + "[2J")
-	gchoice = 9999
-	gchoiceraw = input('[Samurai] What is your command? [press enter to confirm]: ')
+    #print(chr(27) + "[2J")
+    gchoice = 9999
+    gchoiceraw = input('[Samurai] What is your command? [press enter to confirm]: ')
 
-	if gchoiceraw.isdigit():
-		gchoice = int(gchoiceraw)
+    if gchoiceraw.isdigit():
+        gchoice = int(gchoiceraw)
 
-		if gchoice in samuraicmd:
-			runcommand(samuraicmd[gchoice])	
-			print("[Samurai]", samurairespond[gchoice])
-		else:
-			print("[Samurai] Hmm ... your command does not exist, please enter number")
-	else:
-		print("[Samurai] Hmm ... I am confused, please enter a number")
+        if gchoice in samuraicmd:
+            runcommand(samuraicmd[gchoice]) 
+            print("[Samurai]", samurairespond[gchoice])
+        else:
+            print("[Samurai] Hmm ... your command does not exist, please enter number")
+    else:
+        print("[Samurai] Hmm ... I am confused, please enter a number")
 
-	if gchoice == 0:
-		gloop = False
-	elif gchoice == 1:
-		loadoptions()
-		#load the options again. does not work if placed in above array
-	elif gchoice not in samuraicmd:
-		print("[Samurai] Hmm ... I am confused")
+    if gchoice == 0:
+        gloop = False
+    elif gchoice == 1:
+        loadoptions()
+        #load the options again. does not work if placed in above array
+    elif gchoice not in samuraicmd:
+        print("[Samurai] Hmm ... I am confused")
