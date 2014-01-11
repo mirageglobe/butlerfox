49da151cc658f82bc8ce23e6d4bf474a24683e14
Jimmy MG Lim <mirageglobe@gmail.com>
2014-01-11 02:36:02 +0000

    Refactored code so its shorter.

--- samurai.py
+++ samurai.py
@@ -1,74 +1,113 @@
 ﻿# Copyright (C) by Jimmy Mian-Guan Lim (www.mirageglobe.com)
 # MIT License
+# company - http://www.dracoturtur.com
+# author website - http://www.mirageglobe.com
+# project - http://www.dracoturtur.com/samurai
 
-import os, platform
+
+import os, platform, sys
+
+# Samurai command is interactive
+# Ninja command is invisible non interactive
+
+
+ninjacmd 		= {	
+					0:	'',
+					1:	'clear',
+					11: 'sudo apt-get update && apt-get upgrade && apt-get autoremove', 
+					12: 'sudo apt-get dist-upgrade',
+					13: 'sudo apt-get install build-essential',
+					30: 'sudo apt-get install sqlite',
+					31: 'sudo apt-get install nginx',
+					999: ''
+					}
+
+samuraicmd 		= {	
+					0:	'',
+					1:	'clear',
+					11: 'sudo apt-get update && apt-get upgrade && apt-get autoremove', 
+					12: 'sudo apt-get dist-upgrade',
+					13: 'sudo apt-get install build-essential',
+					30: 'sudo apt-get install sqlite',
+					31: 'sudo apt-get install nginx',
+					999: ''
+					}
+
+samuraidesc 	= {	
+					0:	'Exit System',
+					1:	'Clear this screen',
+					11: 'Update ubuntu and cleanup', 
+					12: 'Update ubuntu distribution',
+					13: 'Install build-essential',
+					30: 'Install SQlite',
+					31: 'Install Nginx',
+					999: 'Tell me a joke.. Samurai'
+					}
+
+samurairespond 	= {	
+					0:	'See you later Sensei ... Goodbye',
+					1:	'Screen Cleared',
+					11: 'Command is done',
+					12: 'Command is done',
+					13: 'Command is done',
+					30: 'Command is done',
+					999: 'Joke this! I am serious.'
+					}
+
+
+def loadoptions():
+	print("==================================")	
+	print("Samurai is ready...")
+	print("Detected System: {0} ({1})".format(platform.system(),platform.release()))
+	print("==================================")
+
+	for key, value in samuraidesc.items():
+		print("[", key, "] -", value)
 
 def runcommand(cmdstring):
 	return_value = os.system(cmdstring)
-	if return_value == 0:
-		print "Command Run Success"
+	#if return_value == 0:
+		#print("Command success.")
 
-def startoptions():
-	
-	print "=================================="	
-	print "Welcome to PyLightApp Setup Script"
-	print "MIT(C) MGLim.com"
-	print "=================================="
-	print "System: {0} ({1})".format(platform.system(),platform.release())
-	print "{0}".format(platform.version())
-	print "=--------------------------------="
-	print "[1] Update Ubuntu softwares *Remember to Backup*"
-	print "[2] Update Ubuntu distribution *Remember to Backup*"
-	print "[3] Install Build Essentials"
-	print "[4] Clean up Ubuntu useless files"
-	print "=--------------------------------="
-	print "[5] Install Virtualenv (Via Apt-Get)"
-	print "[6] Install Bottle Python Framework (Via Apt-Get)"
-	print "=--------------------------------="
-	print "[7] Install SQLite"
-	print "=--------------------------------="
-	print "[0] Exit Program"
-	print "=================================="
+# ===============================
+# Finding some commmands
+# ===============================
 
-cmdlist = {50: 'cmd1', 51: 'cmd2'}
+for arg in sys.argv:
+	print(arg)
+# Adding ninja mode soon.
+
+# ===============================
+# Default load of system
+# ===============================
 
 gloop = True
+loadoptions()
+
+# ===============================
+# Entering loop of samurai
+# ===============================
 
 while gloop == True:
-	startoptions()
-
-	gchoice = 99
-	gchoiceraw = raw_input('What is your choice [press enter to confirm]: ')
+	#print(chr(27) + "[2J")
+	gchoice = 9999
+	gchoiceraw = input('[Samurai] What is your command? [press enter to confirm]: ')
 
 	if gchoiceraw.isdigit():
 		gchoice = int(gchoiceraw)
 
+		if gchoice in samuraicmd:
+			runcommand(samuraicmd[gchoice])	
+			print("[Samurai]", samurairespond[gchoice])
+		else:
+			print("[Samurai] Hmm ... your command does not exist, please enter number")
+	else:
+		print("[Samurai] Hmm ... I am confused, please enter a number")
+
 	if gchoice == 0:
-		print "Thanks for using ... Goodbye"
 		gloop = False
 	elif gchoice == 1:
-		print "Update ubuntu softwares via apt-get"
-		runcommand('sudo apt-get update')
-		runcommand('sudo apt-get upgrade')
-	elif gchoice == 2:
-		print "Update ubuntu distribution"
-		runcommand('sudo apt-get dist-upgrade')
-	elif gchoice == 3:
-		print "Install build-essential"
-		runcommand('sudo apt-get install build-essential')
-	elif gchoice == 4:
-		print "Autoremove Cleanup Ubuntu"
-		runcommand('sudo apt-get autoremove')
-	elif gchoice == 5:
-		print "Installing virtualenv"
-		runcommand('sudo apt-get install python-virtualenv')
-	elif gchoice == 6:
-		print "Installing Bottlepy"
-		runcommand('sudo apt-get install python-bottle')
-	elif gchoice == 7:
-		print "Install SQLite "
-		runcommand('sudo apt-get install sqlite')
-	elif gchoice == 99:
-		print "Option does not exists, please enter other number"
-	else:
-		print "Option does not exists, please enter other number"
+		loadoptions()
+		#load the options again. does not work if placed in above array
+	elif gchoice not in samuraicmd:
+		print("[Samurai] Hmm ... I am confused")
