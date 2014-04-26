2da9b2af00c55ee957faa9b054f1d6f1112f2d06
Jimmy MG Lim <mirageglobe@gmail.com>
2014-04-26 11:49:22 +0100

    updated shebang, removed ninjacmd and made options clearer. no functional updates

--- samurai.py
+++ samurai.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 #!/bin/sh
 #exec python3 -x "$0" "$@"
 #!python3
@@ -13,88 +15,144 @@ if sys.version_info < (3, 0):
     sys.stdout.write("Sorry, Samurai requires Python 3.x, not Python 2.x\nYou can install by running sudo apt-get install python3 or run by python3 samurai.py\n")
     sys.exit(1)
 
-
-ninjacmd = { 
-    0:  '',
-    1:  'clear',
-    11: 'sudo apt-get update && apt-get upgrade && apt-get autoremove', 
-    12: 'sudo apt-get dist-upgrade',
-    13: 'sudo apt-get install build-essential',
-    15: 'sudo apt-get install ufw && ufw allow ssh && ufw allow 80 && sudo ufw enable',
-    30: 'sudo apt-get install sqlite',
-    31: 'sudo apt-get install nginx',
-    32: 'sudo apt-get install -y python-software-properties python g++ make && add-apt-repository ppa:chris-lea/node.js && apt-get update && apt-get install nodejs',
-    33: 'sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10 && echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" >> /etc/apt/sources.list.d/mongodb.list && apt-get update && apt-get install mongodb-10gen',
-    50: 'sudo apt-get install php5-fpm php5-mysql',
-    71: 'npm install forever -g',
-    999: ''
-}
-
-samuraicmd = { 
-    0:  '',
-    1:  'clear',
-    11: 'sudo apt-get update && apt-get upgrade && apt-get autoremove', 
-    12: 'sudo apt-get dist-upgrade',
-    13: 'sudo apt-get install build-essential',
-    15: 'sudo apt-get install ufw && ufw allow ssh && ufw allow 80 && sudo ufw enable',
-    19: 'passwd',
-    30: 'sudo apt-get install sqlite',
-    31: 'sudo apt-get remove nginx && apt-get autoremove && -s && nginx=stable && add-apt-repository ppa:nginx/$nginx && apt-get install nginx',
-    32: 'sudo apt-get install -y python-software-properties python g++ make && add-apt-repository ppa:chris-lea/node.js && apt-get update && apt-get install nodejs',
-    33: 'sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10 && echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" >> /etc/apt/sources.list.d/mongodb.list && apt-get update && apt-get install mongodb-10gen',
-    50: 'sudo apt-get install php5-fpm php5-mysql',
-    71: 'npm install forever -g',
-    999: ''
-}
-
-samuraidesc = { 
-    0:  'Exit System',
-    1:  'Clear this screen and show menu',
-    11: 'Update ubuntu and cleanup', 
-    12: 'Update ubuntu distribution',
-    13: 'Install build-essential',
-    15: 'Install and arm ufw (allow ssh 22/80)',
-    19: 'Change password for current user',
-    30: 'Install sqlite',
-    31: 'Install nginx',
-    32: 'Install nodejs and npm',
-    33: 'Install mongodb',
-    50: 'Install php5 (fpm version) and php5 mysql',
-    71: 'Install forever (requires npm)',
-    999: 'Tell me a joke.. Samurai'
-}
-
-samurairespond  = { 
-    0:  'See you later Sensei ... Goodbye',
-    1:  'Screen Cleared',
-    31: 'To start, <service nginx start>; To check config, <nginx -t>',
-    33: 'To start, <service mongodb start>',
-    71: 'To start, go to folder and run <forever start --spinSleepTime 10000 main.js>',
-    999: 'Hai! Joke this! I am serious.'
-}
-
+    
 samuraimap = {}
 
-samuraimap[0] = {   'name': 'Clear Screen',
-                    'cmd': 'clear',
-                    'cmdslient': 'clear',
-                    'responsesuccess': 'Success!',
-                    'responsefail': 'Oh no.'
+# ===============================
+# 1s Core
+# ===============================
+
+samuraimap[0] = {   'name': 'Exit system',
+                    'cmd': '',
+                    'cmdslient': '',
+                    'responsesuccess': 'See you later Sensei ... Goodbye.',
+                    'responsefail': ''
                     }
 
+samuraimap[1] = {   'name': 'Clear screen and show menu',
+                    'cmd': 'clear',
+                    'cmdslient': 'clear',
+                    'responsesuccess': 'Screen Cleared',
+                    'responsefail': ''
+                    }
+
+# ===============================
+# 10s Operating System 
+# ===============================
+
+samuraimap[10] = {  'name': 'Change my login password',
+                    'cmd': 'passwd',
+                    'cmdslient': '',
+                    'responsesuccess': '',
+                    'responsefail': ''
+                    }
+
+samuraimap[11] = {  'name': 'Update ubuntu and cleanup',
+                    'cmd': 'sudo apt-get update && apt-get upgrade && apt-get autoremove',
+                    'cmdslient': '',
+                    'responsesuccess': '',
+                    'responsefail': ''
+                    }
+
+samuraimap[12] = {  'name': 'Update ubuntu distribution',
+                    'cmd': 'sudo apt-get dist-upgrade',
+                    'cmdslient': '',
+                    'responsesuccess': '',
+                    'responsefail': ''
+                    }
+
+samuraimap[13] = {  'name': 'Install build-essential',
+                    'cmd': 'sudo apt-get install build-essential',
+                    'cmdslient': '',
+                    'responsesuccess': '',
+                    'responsefail': ''
+                    }
+
+samuraimap[15] = {  'name': 'Install and arm ufw (allow ssh 22/80)',
+                    'cmd': 'sudo apt-get install ufw && ufw allow ssh && ufw allow 80 && sudo ufw enable',
+                    'cmdslient': '',
+                    'responsesuccess': '',
+                    'responsefail': ''
+                    }
+
+# ===============================
+# 30s Databases
+# ===============================
+
+samuraimap[30] = {  'name': 'Install sqlite',
+                    'cmd': 'sudo apt-get install sqlite',
+                    'cmdslient': '',
+                    'responsesuccess': '',
+                    'responsefail': ''
+                    }
+
+samuraimap[31] = {  'name': 'Install mongodb',
+                    'cmd': 'sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10 && echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" >> /etc/apt/sources.list.d/mongodb.list && apt-get update && apt-get install mongodb-10gen',
+                    'cmdslient': '',
+                    'responsesuccess': 'To start, <service mongodb start>',
+                    'responsefail': ''
+                    }
+
+# ===============================
+# 40s Servers
+# ===============================
+
+samuraimap[41] = {  'name': 'Install nginx',
+                    'cmd': 'sudo apt-get remove nginx && apt-get autoremove && -s && nginx=stable && add-apt-repository ppa:nginx/$nginx && apt-get install nginx',
+                    'cmdslient': '',
+                    'responsesuccess': 'To start, <service nginx start>; To check config, <nginx -t>',
+                    'responsefail': ''
+                    }
+
+# ===============================
+# 50s Platform
+# ===============================
+
+samuraimap[50] = {  'name': 'Install nodejs and npm',
+                    'cmd': 'sudo apt-get install -y python-software-properties python g++ make && add-apt-repository ppa:chris-lea/node.js && apt-get update && apt-get install nodejs',
+                    'cmdslient': '',
+                    'responsesuccess': 'To start, <service nginx start>; To check config, <nginx -t>',
+                    'responsefail': ''
+                    }
+
+# ===============================
+# 60s Languages
+# ===============================
+
+samuraimap[60] = {  'name': 'Install php5 (fpm version) and php5 mysql',
+                    'cmd': 'sudo apt-get install php5-fpm php5-mysql',
+                    'cmdslient': '',
+                    'responsesuccess': 'To start, <service nginx start>; To check config, <nginx -t>',
+                    'responsefail': ''
+                    }
+
+# ===============================
+# 70s Apps
+# ===============================
+
+samuraimap[70] = {  'name': 'Install forever (requires npm)',
+                    'cmd': 'npm install forever -g',
+                    'cmdslient': '',
+                    'responsesuccess': 'To start, go to folder and run <forever start --spinSleepTime 10000 main.js>',
+                    'responsefail': ''
+                    }
+
+# ===============================
+# Functions
+# ===============================
 
 def loadoptions():
     print("==================================") 
     print("Samurai is ready...")
     if ninja_active:
-        print("Ninja mode is armed... All commands are executed sliently (beta)")
+      print("Ninja mode is armed... All commands are executed sliently (beta)")
     print("Detected System: {0} ({1})".format(platform.system(),platform.release()))
     print("Requires: Ubuntu 12.04 above")
     print("==================================")
 
 
-    for key, value in sorted(samuraidesc.items()):
-        print("[", key, "] -", value)
+    for key, value in sorted(samuraimap.items()):
+      print("[", key, "] -", value['name'])
 
 def runcommand(cmdstring):
     return_value = os.system(cmdstring)
@@ -121,6 +179,7 @@ for arg in sys.argv:
 # Default load of system
 # ===============================
 
+avatar = "[samurai]"
 gloop = True
 loadoptions()
 
@@ -128,29 +187,28 @@ loadoptions()
 # Entering loop of samurai
 # ===============================
 
-while gloop == True:
-    #print(chr(27) + "[2J")
-    gchoice = 9999
-    gchoiceraw = input('================================== \n[Samurai] What is your command? \n{enter to confirm | 1 to clear and show menu}: ')
+while gloop:
+  gchoice = 9999
+  gchoiceraw = input("================================== \n{0} What is your command? : ".format(avatar))
 
-    if gchoiceraw.isdigit():
-        gchoice = int(gchoiceraw)
+  if gchoiceraw.isdigit():
+    gchoice = int(gchoiceraw)
 
-        if gchoice in samuraicmd:
-            runcommand(samuraicmd[gchoice]) 
-            if gchoice in samurairespond:
-                print("[Samurai]", samurairespond[gchoice])
-            else:
-                print("[Samurai]", "Command is done")
-        else:
-            print("[Samurai] Hmm ... your command does not exist, please other number")
+    if gchoice in samuraimap:
+      runcommand(samuraimap[gchoice]['cmd']) 
+      if gchoice in samuraimap:
+        print("{0}".format(avatar), samuraimap[gchoice]['responsesuccess'])
+      else:
+        print("{0} Command is complete.".format(avatar))
     else:
-        print("[Samurai] Hmm ... I am confused, please enter a number")
+      print("{0} Command is does not exist. Please enter number.".format(avatar))
+  else:
+    print("{0} Hmm ... I am confused, please enter a number.".format(avatar))
 
-    if gchoice == 0:
-        gloop = False
-    elif gchoice == 1:
-        loadoptions()
-        #load the options again. does not work if placed in above array
-    elif gchoice not in samuraicmd:
-        print("[Samurai] Hmm ... I am confused")
+  if gchoice == 0:
+    gloop = False
+  elif gchoice == 1:
+    loadoptions()
+    #load the options again. does not work if placed in above array
+  elif gchoice not in samuraimap:
+    print("{0} Hmm ... I am confused, please enter a number.".format(avatar))
