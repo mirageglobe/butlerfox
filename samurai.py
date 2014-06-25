c5add34bd9760ec834809dccfba66feee70f3caf
Jimmy MG Lim <mirageglobe@gmail.com>
2014-06-25 19:24:46 +0100

    Added mariadb rule

--- samurai.py
+++ samurai.py
@@ -80,6 +80,13 @@ samuraimap[31] = {  'name': 'Install mongodb',
                     'responsefail': ''
                     }
 
+samuraimap[32] = {  'name': 'Install mariadb',
+                    'cmd': 'sudo apt-get install software-properties-common && sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db && sudo add-apt-repository "deb http://mirrors.coreix.net/mariadb/repo/10.0/ubuntu trusty main" && sudo apt-get update && sudo apt-get install mariadb-server',
+                    'cmdslient': '',
+                    'responsesuccess': '',
+                    'responsefail': ''
+                    }
+
 # ===============================
 # 40s Servers
 # ===============================
