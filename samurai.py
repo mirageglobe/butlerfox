682b77bc0a893923b9d2552790c93d91920138f8
Jimmy MG Lim <mirageglobe@gmail.com>
2014-09-28 12:05:06 +0100

    checks for system type and displays appropriate menus

--- samurai.py
+++ samurai.py
@@ -13,14 +13,16 @@ samuraimap[0] = {   'name': 'Exit system',
                     'cmd': '',
                     'cmdslient': '',
                     'responsesuccess': 'See you later Sensei ... Goodbye.',
-                    'responsefail': ''
+                    'responsefail': '',
+                    'platform':'Darwin ubuntu'
                     }
 
 samuraimap[1] = {   'name': 'Clear screen and show menu',
                     'cmd': 'clear',
                     'cmdslient': 'clear',
                     'responsesuccess': 'Screen Cleared',
-                    'responsefail': ''
+                    'responsefail': '',
+                    'platform':'Darwin ubuntu'
                     }
 
 # ===============================
@@ -31,35 +33,40 @@ samuraimap[10] = {  'name': 'Change my login password',
                     'cmd': 'passwd',
                     'cmdslient': '',
                     'responsesuccess': '',
-                    'responsefail': ''
+                    'responsefail': '',
+                    'platform':'Darwin ubuntu'
                     }
 
 samuraimap[11] = {  'name': 'Update ubuntu and cleanup',
                     'cmd': 'sudo apt-get update && apt-get upgrade && apt-get autoremove',
                     'cmdslient': '',
                     'responsesuccess': '',
-                    'responsefail': ''
+                    'responsefail': '',
+                    'platform':'ubuntu'
                     }
 
 samuraimap[12] = {  'name': 'Update ubuntu distribution',
                     'cmd': 'sudo apt-get dist-upgrade',
                     'cmdslient': '',
                     'responsesuccess': '',
-                    'responsefail': ''
+                    'responsefail': '',
+                    'platform':'ubuntu'
                     }
 
 samuraimap[13] = {  'name': 'Install build-essential',
                     'cmd': 'sudo apt-get install build-essential',
                     'cmdslient': '',
                     'responsesuccess': '',
-                    'responsefail': ''
+                    'responsefail': '',
+                    'platform':'ubuntu'
                     }
 
 samuraimap[15] = {  'name': 'Install and arm ufw (allow ssh 22/80)',
                     'cmd': 'sudo apt-get install ufw && ufw allow ssh && ufw allow 80 && sudo ufw enable',
                     'cmdslient': '',
                     'responsesuccess': '',
-                    'responsefail': ''
+                    'responsefail': '',
+                    'platform':'ubuntu'
                     }
 
 # ===============================
@@ -70,21 +77,24 @@ samuraimap[30] = {  'name': 'Install sqlite',
                     'cmd': 'sudo apt-get install sqlite',
                     'cmdslient': '',
                     'responsesuccess': '',
-                    'responsefail': ''
+                    'responsefail': '',
+                    'platform':'ubuntu'
                     }
 
 samuraimap[31] = {  'name': 'Install mongodb',
                     'cmd': 'sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10 && echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" >> /etc/apt/sources.list.d/mongodb.list && apt-get update && apt-get install mongodb-10gen',
                     'cmdslient': '',
                     'responsesuccess': 'To start, <service mongodb start>',
-                    'responsefail': ''
+                    'responsefail': '',
+                    'platform':'ubuntu'
                     }
 
 samuraimap[32] = {  'name': 'Install mariadb',
                     'cmd': 'sudo apt-get install software-properties-common && sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db && sudo add-apt-repository "deb http://mirrors.coreix.net/mariadb/repo/10.0/ubuntu trusty main" && sudo apt-get update && sudo apt-get install mariadb-server',
                     'cmdslient': '',
                     'responsesuccess': '',
-                    'responsefail': ''
+                    'responsefail': '',
+                    'platform':'ubuntu'
                     }
 
 # ===============================
@@ -95,7 +105,8 @@ samuraimap[41] = {  'name': 'Install nginx',
                     'cmd': 'sudo apt-get install python-software-properties && add-apt-repository ppa:nginx/stable && apt-get update && apt-get install nginx',
                     'cmdslient': '',
                     'responsesuccess': 'To start, <service nginx start>; To check config, <nginx -t>',
-                    'responsefail': ''
+                    'responsefail': '',
+                    'platform':'ubuntu'
                     }
 
 # ===============================
@@ -106,7 +117,8 @@ samuraimap[50] = {  'name': 'Install nodejs and npm',
                     'cmd': 'sudo apt-get install -y python-software-properties python g++ make && add-apt-repository ppa:chris-lea/node.js && apt-get update && apt-get install nodejs',
                     'cmdslient': '',
                     'responsesuccess': 'To install npm packages: <npm install>; To run node scripts: <node myscript.js>',
-                    'responsefail': ''
+                    'responsefail': '',
+                    'platform':'ubuntu'
                     }
 
 # ===============================
@@ -117,7 +129,8 @@ samuraimap[60] = {  'name': 'Install php5 (fpm version) and php5 mysql',
                     'cmd': 'sudo apt-get install php5-fpm php5-mysql',
                     'cmdslient': '',
                     'responsesuccess': 'To start, <service nginx start>; To check config, <nginx -t>',
-                    'responsefail': ''
+                    'responsefail': '',
+                    'platform':'ubuntu'
                     }
 
 # ===============================
@@ -128,7 +141,8 @@ samuraimap[70] = {  'name': 'Install forever (requires npm)',
                     'cmd': 'npm install forever -g',
                     'cmdslient': '',
                     'responsesuccess': 'To start, go to folder and run <forever start --spinSleepTime 10000 main.js>',
-                    'responsefail': ''
+                    'responsefail': '',
+                    'platform':'ubuntu'
                     }
 
 # ===============================
@@ -140,16 +154,15 @@ samuraimap[70] = {  'name': 'Install forever (requires npm)',
 # Functions
 # ===============================
 
-def loadoptions():
+def loadoptions(ninja=False):
   print("==================================") 
-  print("Samurai is ready...")
   print("Detected System: {0} ({1})".format(platform.system(),platform.release()))
-  if ninja_active:
-    print("Ninja mode is armed... All commands are executed sliently (beta)")
+  print("Ninja Mode: {0}".format(ninja))
   print("==================================")
 
   for key, value in sorted(samuraimap.items()):
-    print("[", key, "] -", value['name'])
+    if platform.system() in value['platform']:
+      print("[", key, "] -", value['name'])
 
 def runcommand(cmdstring):
   return_value = os.system(cmdstring)
@@ -176,12 +189,7 @@ if __name__ == "__main__":
 
   ninja_active = False
 
-  for arg in sys.argv:
-    #print(arg)
-    if arg == "-ninja":
-      ninja_active = True
-
-  # Adding ninja mode here
+  # Adding ninja mode here - ninja mode activated within menu
   # In ninja mode, the commands are not executed; until the end. if you run ninja, it will create a scroll.sh with bash commands which can be used with vagrant
   # export DEBIAN_FRONTEND=noninteractive
   # apt-get -y install package1 package2
