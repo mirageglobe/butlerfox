13af448733c10bf3d4bbe0b827ed04ed77e98e21
Jimmy Mian-Guan Lim <mirageglobe@gmail.com>
2015-02-11 13:29:34 +0000

    created platform specific versions for clarity

--- samuraimac.py
+++ samuraimac.py
@@ -13,7 +13,6 @@ samuraimap[0] = {   'name': 'Exit system',
                     'cmdslient': '',
                     'responsesuccess': '',
                     'responsefail': '',
-                    'platform':'Linux'
                     }
 
 samuraimap[1] = {   'name': 'Clear screen and show menu',
@@ -21,7 +20,6 @@ samuraimap[1] = {   'name': 'Clear screen and show menu',
                     'cmdslient': 'clear',
                     'responsesuccess': 'Screen Cleared',
                     'responsefail': '',
-                    'platform':'Linux'
                     }
 
 samuraimap[2] = {   'name': 'Toggle Ninja Mode',
@@ -29,7 +27,6 @@ samuraimap[2] = {   'name': 'Toggle Ninja Mode',
                     'cmdslient': '',
                     'responsesuccess': '',
                     'responsefail': '',
-                    'platform':'Linux'
                     }
 
 samuraimap[3] = {   'name': 'Toggle ShowCmd Mode',
@@ -37,7 +34,13 @@ samuraimap[3] = {   'name': 'Toggle ShowCmd Mode',
                     'cmdslient': '',
                     'responsesuccess': '',
                     'responsefail': '',
-                    'platform':'Linux'
+                    }
+
+samuraimap[4] = {   'name': 'Show Stats Mem',
+                    'cmd': 'top -l 1 | head -n 10 | grep PhysMem',
+                    'cmdslient': '',
+                    'responsesuccess': '',
+                    'responsefail': '',
                     }
 
 samuraimap[7] = {   'name': 'Show IP Address',
@@ -45,7 +48,6 @@ samuraimap[7] = {   'name': 'Show IP Address',
                     'cmdslient': '',
                     'responsesuccess': '',
                     'responsefail': '',
-                    'platform':'Linux'
                     }
 
 samuraimap[8] = {   'name': 'Update Samurai.py',
@@ -53,7 +55,6 @@ samuraimap[8] = {   'name': 'Update Samurai.py',
                     'cmdslient': '',
                     'responsesuccess': '',
                     'responsefail': '',
-                    'platform':'Linux'
                     }
 
 samuraimap[9] = {   'name': 'Remove Samurai',
@@ -61,7 +62,6 @@ samuraimap[9] = {   'name': 'Remove Samurai',
                     'cmdslient': '',
                     'responsesuccess': 'Samurai removed.',
                     'responsefail': '',
-                    'platform':'Darwin Linux'
                     }
 
 # ===============================
@@ -73,124 +73,34 @@ samuraimap[10] = {  'name': 'Change my login password',
                     'cmdslient': '',
                     'responsesuccess': '',
                     'responsefail': '',
-                    'platform':'Darwin Linux'
-                    }
-
-samuraimap[11] = {  'name': 'Update ubuntu and cleanup',
-                    'cmd': 'sudo apt-get update && apt-get upgrade && apt-get autoremove',
-                    'cmdslient': '',
-                    'responsesuccess': '',
-                    'responsefail': '',
-                    'platform':'Linux'
-                    }
-
-samuraimap[12] = {  'name': 'Update ubuntu distribution',
-                    'cmd': 'sudo apt-get dist-upgrade',
-                    'cmdslient': '',
-                    'responsesuccess': '',
-                    'responsefail': '',
-                    'platform':'Linux'
-                    }
-
-samuraimap[13] = {  'name': 'Install build-essential',
-                    'cmd': 'sudo apt-get install build-essential',
-                    'cmdslient': '',
-                    'responsesuccess': '',
-                    'responsefail': '',
-                    'platform':'Linux'
-                    }
-
-samuraimap[14] = {  'name': 'Install and arm ufw (allow ssh 22/80)',
-                    'cmd': 'sudo apt-get install ufw && ufw allow ssh && ufw allow 80 && sudo ufw enable',
-                    'cmdslient': '',
-                    'responsesuccess': '',
-                    'responsefail': '',
-                    'platform':'Linux'
-                    }
-
-samuraimap[15] = {  'name': 'Add virtualbox guest additions for ubuntu',
-                    'cmd': 'sudo apt-get install virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11',
-                    'cmdslient': '',
-                    'responsesuccess': '',
-                    'responsefail': '',
-                    'platform':'Linux'
                     }
 
 # ===============================
 # 30s Databases
 # ===============================
 
-samuraimap[30] = {  'name': 'Install sqlite',
-                    'cmd': 'sudo apt-get install sqlite',
-                    'cmdslient': '',
-                    'responsesuccess': '',
-                    'responsefail': '',
-                    'platform':'Linux'
-                    }
-
-samuraimap[31] = {  'name': 'Install mongodb',
-                    'cmd': 'sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10 && echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" >> /etc/apt/sources.list.d/mongodb.list && apt-get update && apt-get install mongodb-10gen',
-                    'cmdslient': '',
-                    'responsesuccess': 'To start, <service mongodb start>',
-                    'responsefail': '',
-                    'platform':'Linux'
-                    }
-
-samuraimap[32] = {  'name': 'Install mariadb (for ubuntu 14.04)',
-                    'cmd': 'sudo apt-get install software-properties-common && sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db && sudo add-apt-repository "deb http://lon1.mirrors.digitalocean.com/mariadb/repo/10.0/ubuntu trusty main" && sudo apt-get update && sudo apt-get install mariadb-server',
-                    'cmdslient': '',
-                    'responsesuccess': '',
-                    'responsefail': '',
-                    'platform':'Linux'
-                    }
-
 # ===============================
 # 40s Servers
 # ===============================
-
-samuraimap[41] = {  'name': 'Install nginx',
-                    'cmd': 'sudo apt-get install python-software-properties && add-apt-repository ppa:nginx/stable && apt-get update && apt-get install nginx',
-                    'cmdslient': '',
-                    'responsesuccess': 'To start, <service nginx start>; To check config, <nginx -t>',
-                    'responsefail': '',
-                    'platform':'Linux'
-                    }
 
 # ===============================
 # 50s Platform and Pkg Managers
 # ===============================
 
-samuraimap[50] = {  'name': 'Install nodejs and npm',
-                    'cmd': 'sudo apt-get install -y python-software-properties python g++ make && add-apt-repository ppa:chris-lea/node.js && apt-get update && apt-get install nodejs',
+samuraimap[51] = {  'name': 'Install HomeBrew',
+                    'cmd': 'ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"',
                     'cmdslient': '',
-                    'responsesuccess': 'To install npm packages: <npm install>; To run node scripts: <node myscript.js>',
+                    'responsesuccess': '',
                     'responsefail': '',
-                    'platform':'Linux'
                     }
 
 # ===============================
 # 60s Languages
 # ===============================
 
-samuraimap[60] = {  'name': 'Install php5 (fpm version) with MySQL/MariaDB',
-                    'cmd': 'sudo apt-get install php5-fpm php5-mysql php5-cli',
-                    'cmdslient': '',
-                    'responsesuccess': 'To start, <service nginx start>; To check config, <nginx -t>; refer to https://www.digitalocean.com/community/tutorials/how-to-install-linux-nginx-mysql-php-lemp-stack-on-ubuntu-14-04; remember to (1) edit /etc/php5/fpm/php.ini to set cgi.fix_pathinfo=0 and (2) /etc/nginx/sites-available/default to root /usr/share/nginx/html; Also to uncomment php location to use phpfpm; to test just drop a php file in the nginx html folder',
-                    'responsefail': '',
-                    'platform':'Linux'
-                    }
-
 # ===============================
 # 70s Apps
 # ===============================
-
-samuraimap[70] = {  'name': 'Install forever (requires npm)',
-                    'cmd': 'npm install forever -g',
-                    'cmdslient': '',
-                    'responsesuccess': 'To start, go to folder and run <forever start --spinSleepTime 10000 main.js>',
-                    'responsefail': '',
-                    'platform':'Linux'
-                    }
 
 # ===============================
 # Custom Apps Start from 100
@@ -202,8 +112,8 @@ samuraimap[70] = {  'name': 'Install forever (requires npm)',
 # ===============================
 
 def loadoptions(ninja=False,showcmd=False):
-  print("==================================")
-  print("Samurai Linux") 
+  print("==================================") 
+  print("Samurai Mac") 
   print("Detected System: {0} ({1}) - showing compatible commands".format(platform.system(),platform.release()))
   print("Ninja Mode: {0}".format(ninja))
   print("Show Cmd: {0}".format(showcmd))
@@ -217,7 +127,6 @@ def loadoptions(ninja=False,showcmd=False):
 
 def runcommand(cmdstring):
   return_value = os.system(cmdstring)
-
 
 # ===============================
 # Main Code
@@ -233,9 +142,17 @@ if __name__ == "__main__":
     sys.stdout.write("[Samurai] Samurai requires Python 3.x, and you are running it as Python 2.x\n")
     sys.stdout.write("[Samurai] You can install by running sudo apt-get install python3 or python3 samurai.py\n")
     sys.exit(1)
+
+  # ===============================
+  # Checks for system
+  # ===============================
+
+  if platform.system() != 'Darwin':
+    print("[!] System incompatible, please run samurai.py instead.")
+    sys.exit(1)
   
   # ===============================
-  # Checking arguments
+  # Setting arguments
   # ===============================
 
   ninja_active = False
