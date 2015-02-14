b63a8102475d79a767843d4806f97c485d4685c5
Jimmy Mian-Guan Lim <mirageglobe@gmail.com>
2015-02-14 18:59:30 +0000

    updated install script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Installs samurai into /usr/local/bin
-# Files: samurai.py and samuraimac.py
+# Files: samurai / samurai-mac.py / samurai-linux.py
 
 if [[ $(id -u) != 0 ]]; then
   if command -v sudo >/dev/null 2>&1; then
@@ -12,10 +12,14 @@ if [[ $(id -u) != 0 ]]; then
   fi
 fi
 
-if git ls-files >& /dev/null && [[ -f samurai.py ]]; then
-  $SUDO cp samurai.py /usr/local/bin/samurai.py || { echo "Failed to install Samurai into /usr/local/bin."; exit 1; }
+if git ls-files >& /dev/null && [[ -f samurai ]]; then
+  $SUDO cp samurai /usr/local/bin/samurai || { echo "Failed to install samurai into /usr/local/bin."; exit 1; }
+  $SUDO cp samurai-mac.py /usr/local/bin/samurai-mac.py || { echo "Failed to install samurai-mac.py into /usr/local/bin."; exit 1; }
+  $SUDO cp samurai-linux.py /usr/local/bin/samurai-linux.py || { echo "Failed to install samurai-linux.py into /usr/local/bin."; exit 1; }
 else
-  $SUDO curl -L https://raw.githubusercontent.com/mirageglobe/samurai/master/samurai.py -o /usr/local/bin/samurai.py && $SUDO chmod +x /usr/local/bin/samurai.py || { echo "Failed to install Samurai into /usr/local/bin."; exit 1; }
+  $SUDO curl -L https://raw.githubusercontent.com/mirageglobe/samurai/master/samurai -o /usr/local/bin/samurai && $SUDO chmod +x /usr/local/bin/samurai || { echo "Failed to install samurai into /usr/local/bin."; exit 1; }
+  $SUDO curl -L https://raw.githubusercontent.com/mirageglobe/samurai/master/samurai-mac.py -o /usr/local/bin/samurai-mac.py && $SUDO chmod +x /usr/local/bin/samurai-mac.py || { echo "Failed to install samurai-mac.py into /usr/local/bin."; exit 1; }
+  $SUDO curl -L https://raw.githubusercontent.com/mirageglobe/samurai/master/samurai-linux.py -o /usr/local/bin/samurai-linux.py && $SUDO chmod +x /usr/local/bin/samurai-linux.py || { echo "Failed to install samurai-linux.py into /usr/local/bin."; exit 1; }
 fi
 
-echo "Installed Samurai into /usr/local/bin."; exit 0;
+echo "Installed Samurai into /usr/local/bin. To uninstall, delete samurai/samurai-mac.py/samurai-linux.py from folder."; exit 0;
