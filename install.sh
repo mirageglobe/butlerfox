e0851a3dad66f1bd2663b610e7a385add1d60bcf
Jimmy Mian-Guan Lim <mirageglobe@gmail.com>
2015-05-09 13:05:25 +0100

    beautified installer

--- install.sh
+++ install.sh
@@ -3,33 +3,99 @@
 # installs samurai into /usr/local/bin
 # files: samurai / samurai-mac.py / samurai-linux.py
 
+# ----- includebash.sh start
+
+# ----- information
+# version 1.0.0
+
+# ----- notes
+# [›] usage:
+#     source "./includebash.sh"
+#     or
+      # if config file exists, use the variables.
+#     if [ -f includebash.sh ]; then
+#       source includebash.sh
+#     else
+#       echo "[!] warning - includebash.sh not found. things may look uglier."
+#     fi
+
+# [›] limitations: 
+#     - script has to be in same directory of running
+
+# [›] tips: 
+#     - exit code such as exit 0 or exit 1 in bash. 0 is successful exit, and 1 or more is failed exit
+
+# ----- functions
+
+spinner()
+{
+  local pid=$1
+  local delay=0.75
+  local spinstr='|/-\'
+  while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+    local temp=${spinstr#?}
+    printf " [%c]  " "$spinstr"
+    local spinstr=$temp${spinstr%"$temp"}
+    sleep $delay
+    printf "\b\b\b\b\b\b"
+  done
+  printf "    \b\b\b\b"
+}
+
+# define a timestamp function
+timestamp() 
+{
+  date +"%T"
+}
+
+datestamp()
+{
+  date +"%Y%m%d%H%M%S"
+}
+
+# ----- variables
+
+DTTITLE="[›]"
+DTTEXT="   "
+
+# ----- includebash.sh end
+
+
+# checking sudo
+
+echo "$DTTITLE checking sudo"
+
 if [[ $(id -u) != 0 ]]; then
   if command -v sudo >/dev/null 2>&1; then
     SUDO="sudo"
   else
-    echo "Sudo not installed. Aborting."
+    echo "$DTTEXT sudo not installed. aborting."
     exit 1
   fi
 fi
 
 # remove old samurai files
 
+echo "$DTTITLE removing old samurai files"
+
 if [ -f /usr/local/bin/samurai ]; then
-  echo "found and removing /usr/local/bin/samurai file"
+  echo "$DTTEXT found and removing /usr/local/bin/samurai file"
   sudo rm /usr/local/bin/samurai
 fi
 
 if [ -f /usr/local/bin/samurai-mac ]; then
-  echo "found and removing /usr/local/bin/samurai-mac file"
+  echo "$DTTEXT found and removing /usr/local/bin/samurai-mac file"
   sudo rm /usr/local/bin/samurai-mac.py
 fi
 
 if [ -f /usr/local/bin/samurai-linux ]; then
-  echo "found and removing /usr/local/bin/samurai-linux file"
+  echo "$DTTEXT found and removing /usr/local/bin/samurai-linux file"
   sudo rm /usr/local/bin/samurai-linux.py
 fi
 
 # install new samurai files
+
+echo "$DTTITLE installing samurai"
 
 if git ls-files >& /dev/null && [[ -f samurai ]]; then
   # if installing via git pull/clone
@@ -46,4 +112,8 @@ else
   $SUDO chmod g+x /usr/local/bin/samurai-linux.py || { echo "Failed to install samurai-linux.py into /usr/local/bin."; exit 1; }
 fi
 
-echo "Installed Samurai into /usr/local/bin. To uninstall, delete samurai/samurai-mac.py/samurai-linux.py from folder."; exit 0;
+# summary
+
+echo "$DTTITLE summary"
+
+echo "$DTTEXT installed samurai into /usr/local/bin. To uninstall, delete samurai/samurai-mac.py/samurai-linux.py from folder." exit 0
